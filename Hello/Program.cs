86087650f06f3caa0d9ddbbeb863c2801d56cd53
04an <olegdzhum@gmail.com>
2019-10-14 16:21:45 +0200

    Dodaj pliki projektów.

---
+++ Hello/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            int a;
+            a = 5;
+
+            /*sbyte = -128 do 127, na 8 bit, 1 bajcie\
+             * short = -32,768 do 32,767, na 16 bit, 2 bajtach
+             * 
+             * przefiks u przed typem oznacza brak zakresu liczb z minusem
+             * ushort = 0 do 65.55
+             * 
+             * float = 7 znak po przecinki;
+             * double od 15 do 16 znakow po przecinku;
+             * decimal = 28 znakow po przecinku;
+             */
+            
+
+            Console.WriteLine(a);
+            Console.ReadLine();
+            
+
+          
+        }
+    }
+}
